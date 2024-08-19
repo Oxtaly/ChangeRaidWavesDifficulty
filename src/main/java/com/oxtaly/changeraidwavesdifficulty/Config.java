d496cf2f28dce9c7fb04fbd978156ed9290b98e4
Oxtaly <Oxtaly@gmail.com>
2024-08-19 11:40:41 +0200

    cleaned up code & licence change

--- src/main/java/com/oxtaly/changeraidwavesdifficulty/Config.java
+++ src/main/java/com/oxtaly/changeraidwavesdifficulty/Config.java
@@ -1,12 +1,8 @@
 package com.oxtaly.changeraidwavesdifficulty;
 
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.config.ModConfigEvent;
 
-// An oxtaly config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = ChangeRaidWavesDifficulty.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
@@ -16,10 +12,4 @@ public class Config {
             .defineInRange("raidDifficulty", -1, -1, 3);
 
     static final ForgeConfigSpec SPEC = BUILDER.build();
-    // public static int raidDifficulty;
-
-    // @SubscribeEvent
-    // static void onLoad(final ModConfigEvent event) {
-    //     raidDifficulty = RaidDifficulty.get();
-    // }
 }
--- src/main/java/com/oxtaly/changeraidwavesdifficulty/commands/SetDifficultyCommand.java
+++ src/main/java/com/oxtaly/changeraidwavesdifficulty/commands/SetDifficultyCommand.java
@@ -1,29 +1,17 @@
 package com.oxtaly.changeraidwavesdifficulty.commands;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.context.CommandContext;
-import com.oxtaly.changeraidwavesdifficulty.ChangeRaidWavesDifficulty;
-
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.oxtaly.changeraidwavesdifficulty.Config;
-import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Difficulty;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.raid.Raid;
-import net.minecraft.world.entity.raid.Raider;
 
 import javax.annotation.Nullable;
-import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 public class SetDifficultyCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -37,44 +25,6 @@ public class SetDifficultyCommand {
                         return false;
                     }
                 })
-                // Debug commands!
-                // .then(Commands.literal("stopraid")
-                //         .executes(css -> {
-                //             ServerPlayer player = css.getSource().getPlayerOrException();
-                //             ServerLevel level = player.serverLevel();
-                //             Raid raid = level.getRaidAt(player.blockPosition());
-                //
-                //             if(raid == null) {
-                //                 css.getSource().sendFailure(Component.literal("You must be near a Raid to use this command!"));
-                //                 return 0;
-                //             }
-                //
-                //             List<Raider> aliveRaiders = raid.getAllRaiders().stream().filter(LivingEntity::isAlive).toList();
-                //             aliveRaiders.forEach(LivingEntity::kill);
-                //             raid.stop();
-                //
-                //             css.getSource().sendSuccess(() -> Component.literal("Stopped nearby raid!"), true);
-                //             return 1;
-                //         })
-                // )
-                // .then(Commands.literal("nextwave")
-                //         .executes(css -> {
-                //             ServerPlayer player = css.getSource().getPlayerOrException();
-                //             ServerLevel level = player.serverLevel();
-                //             Raid raid = level.getRaidAt(player.blockPosition());
-                //
-                //             if(raid == null) {
-                //                 css.getSource().sendFailure(Component.literal("You must be near a Raid to use this command!"));
-                //                 return 0;
-                //             }
-                //
-                //             List<Raider> aliveRaiders = raid.getAllRaiders().stream().filter(LivingEntity::isAlive).toList();
-                //             aliveRaiders.forEach(LivingEntity::kill);
-                //
-                //             css.getSource().sendSuccess(() -> Component.literal("Cleared current raid wave!"), true);
-                //             return 1;
-                //         })
-                // )
                 .then(Commands.literal("setdifficulty")
                         .then(Commands.literal("default").executes((ctx) -> SetDifficultyCommand.setDifficulty(ctx, null)))
                         .then(Commands.literal("peaceful").executes((ctx) -> SetDifficultyCommand.setDifficulty(ctx, Difficulty.PEACEFUL)))
@@ -83,20 +33,6 @@ public class SetDifficultyCommand {
                         .then(Commands.literal("hard").executes((ctx) -> SetDifficultyCommand.setDifficulty(ctx, Difficulty.HARD)))
 
                 );
-                // .then(Commands.literal("getdifficulty")
-                //         .executes((ctx) -> {
-                //             int configdifficulty = Config.RaidDifficulty.get();
-                //             ChangeRaidWavesDifficulty.LOGGER.info("Hello from config difficulty: {}", configdifficulty);
-                //             ctx.getSource().sendSuccess(() -> Component.literal("Hello from config difficulty: ").append(Integer.toString(configdifficulty)), true);
-                //             return 1;
-                //         })
-                //
-                // );
-                // .executes(css -> {
-                //     ChangeRaidWavesDifficulty.LOGGER.info("Hello from command!");
-                //     css.getSource().sendSuccess(() -> Component.literal("Hello from command!"), true);
-                //     return 1;
-                // });
 
         dispatcher.register(literalArgumentBuilder);
     }
